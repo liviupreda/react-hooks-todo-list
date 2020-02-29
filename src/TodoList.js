4db1bac0627aa15a902722126549d005c26d6640
Liviu Preda <liviupreda@gmail.com>
2020-02-29 10:05:35 +0200

    render todo tasks in TodoList using MaterialUI components

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 export default function TodoList(props) {
   return (
     <Paper>
-      {props.todos.map((todo, i) => (
-        <li key={i}>{todo.task}</li>
-      ))}
+      <List>
+        {props.todos.map((todo, i) => (
+          <>
+            <ListItem key={i}>
+              <ListItemText>{todo.task}</ListItemText>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
     </Paper>
   );
 }
