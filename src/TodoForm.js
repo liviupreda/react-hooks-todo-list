20066486c66cb3696af47db786f5a308ae4cfc81
Liviu Preda <liviupreda@gmail.com>
2020-02-29 11:11:57 +0200

    make TodoForm controlled by importing the useInputState hook

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
+import useInputState from "./hooks/useInputState";
 
 export default function TodoForm(props) {
+  const [value, handleChange, reset] = useInputState("");
   return (
     <Paper>
-      <TextField />
+      {value}
+      <TextField value={value} onChange={handleChange} />
     </Paper>
   );
 }
