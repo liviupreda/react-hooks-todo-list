19170f46d9476b797962e2fb08ff940360986b8f
Liviu Preda <liviupreda@gmail.com>
2020-02-29 08:18:54 +0200

    import required MaterialUI components in TodoApp

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
 export default function TodoApp() {
   return <h1>Todo List</h1>;
