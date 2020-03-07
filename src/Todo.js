0e2ff0a51a1088b535f3f4a64083540f97421d4b
Liviu Preda <liviupreda@gmail.com>
2020-03-07 10:29:42 +0200

    add Checkboxes

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
 
 function Todo({ task, completed }) {
   return (
     <ListItem>
+      {/* tabindex of -1 => the control does not receive focus when using the tab key. */}
+      <Checkbox tabIndex={-1} checked={completed} />
       <ListItemText>{task}</ListItemText>
     </ListItem>
   );
