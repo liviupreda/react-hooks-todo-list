7c5df42c6f929c8443aeb0b12ce42611d168fcc2
Liviu Preda <liviupreda@gmail.com>
2020-04-01 08:53:23 +0300

    add conditional rendering based on isEditing state

--- src/Todo.js
+++ src/Todo.js
@@ -14,25 +14,31 @@ function Todo({ id, task, completed, removeTodo, toggleTodo }) {
   const [isEditing, toggle] = useToggleState(false);
   return (
     <ListItem>
-      {/* tabindex of -1 => the control does not receive focus when using the tab key. */}
-      <Checkbox
-        tabIndex={-1}
-        checked={completed}
-        onClick={() => toggleTodo(id)}
-      />
-      <ListItemText
-        style={{ textDecoration: completed ? "line-through" : "none" }}
-      >
-        {task}
-      </ListItemText>
-      <ListItemSecondaryAction>
-        <IconButton aria-label="Delete" onClick={() => removeTodo(id)}>
-          <DeleteIcon />
-        </IconButton>
-        <IconButton aria-label="Edit">
-          <EditIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
+      {isEditing ? (
+        <h1>Editing time! </h1>
+      ) : (
+        <>
+          {/* tabindex of -1 => the control does not receive focus when using the tab key. */}
+          <Checkbox
+            tabIndex={-1}
+            checked={completed}
+            onClick={() => toggleTodo(id)}
+          />
+          <ListItemText
+            style={{ textDecoration: completed ? "line-through" : "none" }}
+          >
+            {task}
+          </ListItemText>
+          <ListItemSecondaryAction>
+            <IconButton aria-label="Delete" onClick={() => removeTodo(id)}>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label="Edit">
+              <EditIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </>
+      )}
     </ListItem>
   );
 }
